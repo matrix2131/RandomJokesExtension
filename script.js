d668b86ba2153ca02c538061b0dfdec62755addc
Soniya <70225955+matrix2131@users.noreply.github.com>
2023-01-31 18:00:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById('jokeElement');
+
+        jokeElement.innerHTML = jokeText;  // The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+    })
